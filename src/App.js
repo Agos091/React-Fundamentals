e6c1f75aae8e842366dc319d9798628c742c86b8
Agos Dalcin Rufino <agos.dalcin@martinellilabs.com.br>
2024-11-18 16:23:53 -0300

    Just testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
+
+const category = "Salve";
+
 function App() {
-  return <h1>First Component</h1>;
+  return (
+    <>
+      <h1>First Component </h1>
+      <h2>{category}</h2>
+    </>
+  );
 }
 export default App;
